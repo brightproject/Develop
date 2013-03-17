010051edc57f0a5ac37cddea9e6e83ca55bc0776
Brightproject <brightproject@gmail.com>
2013-03-17 23:52:14 +0300

    Файл растет:)

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 		require_once ("safemysql.class.php");
 		$db = new SafeMySQL();
-		$name = "Petr";
-		$user = $db->getRow("SELECT * FROM first where name=?s",$_GET['name']);
-		echo $user;
+		$profile .= "<form action=\"index.php\" method=\"get\">";
+		$profile .= "<p><b>Введите имя:</b></p>";
+		$profile .= "<p><textarea rows=\"1\" cols=\"45\" name=\"name\"></textarea></p>";
+		$profile .= "<p><input type=\"submit\" value=\"Отправить\"></p>";
+		$profile .= "</form>";
+		$user = $db->getRow("SELECT * FROM first where name=?s", $_GET['name']);
+		$profile .= "<ul>";
+				 foreach ($user as $m)
+					 {
+						$profile .= "<ol>".$m."</ol>";
+					 }
+		$profile .= "</ul>";
+		echo $profile;
 ?>
