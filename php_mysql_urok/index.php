e5047a0f4c8c824c7e5477629c31f78248099dae
Brightproject <brightproject@gmail.com>
2013-03-25 15:14:25 +0300

    Редактирую индекс, смених доктайп на правильный

--- php_mysql_urok/index.php
+++ php_mysql_urok/index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru" dir="ltr">
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-	<meta http-equiv="Content-Type" con	tent="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Изучаем PHP</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
   </head>
   <body>
   	<div id="page-wrap">
-    <?php
-	
+    <?php 
       include_once('class/simpleCMS.php'); // подключаем файл с классом
       $obj = new simpleCMS(); // создаем объект класса управления
 	  $db_connection = $obj->connectDB();
@@ -17,11 +16,11 @@
       if( $_GET['admin'] == 1 ){ // если есть в URL переменная admin
 	    print $obj->display_admin(); // если есть переменная, то отображаем форму
 	  }else{
-	    print $obj->display_public(); // если переменной нет, то отображаем сообщения
+	    print $obj->display_public_DB(); // если переменной нет, то отображаем сообщения
 	  }
 	  
 	  if($_POST){
-        $obj->write($_POST);
+        $obj->write_DB($_POST);
 	  }
 	  
 	  mysql_close($db_connection);
