a28cdd4d03950113d69711afd3798c18b138ce51
Brightproject <brightproject@gmail.com>
2013-03-25 17:30:17 +0300

    сменил условия выбора add,update,delete

--- php_mysql_urok/index.php
+++ php_mysql_urok/index.php
@@ -16,10 +16,30 @@
       $obj = new simpleCMS(); // создаем объект класса управления
 	  $db_connection = $obj->connectDB();
 	  $obj->display_tables();
-      if( $_GET['admin'] == 1 ){ // если есть в URL переменная admin
-	    print $obj->display_admin(); // если есть переменная, то отображаем форму
-	  }else{
-	    print $obj->display_public_DB(); // если переменной нет, то отображаем сообщения
+      // if( $_GET['admin'] == 1 ){ // если есть в URL переменная admin
+	    // print $obj->display_admin(); // если есть переменная, то отображаем форму
+	  // }else{
+	    // print $obj->display_public_DB(); // если переменной нет, то отображаем сообщения
+	  // }
+	  
+	  switch ($_GET['admin']){
+	    case 'add':
+		  print $obj->display_admin(); // если переменная равна 1, то отображаем форму добавления
+		break;
+		case 'update':
+		  print $obj->display_update();  // если переменная равна update, то отображаем форму изменения
+		break;
+		case 'delete':
+		  if($_GET['mid']){ //если переменная ровна delete, то проверяем наличие mid
+			$obj->delete_DB($_GET['mid']); //вызываем метод удаления сообщения
+			print $obj->display_public_DB(); // если переменной нет, то отображаем сообщения
+		  }else{
+			print '<p>Не выбран mid!</p>';
+			print $obj->display_public_DB(); // отображаем список сообщений			
+		  }
+		break;
+		default:
+		  print $obj->display_public_DB(); // если переменной нет, то отображаем сообщения
 	  }
 	  
 	  if($_POST){
