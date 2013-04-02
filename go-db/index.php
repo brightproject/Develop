b845c4fd44cfd6fca111d3be0180decf4215a410
Brightproject <brightproject@gmail.com>
2013-04-02 15:51:21 +0300

    подключение к БД

--- go-db/index.php
+++ go-db/index.php
@@ -6,6 +6,16 @@ error_reporting(E_ALL);
 header('Content-Type: text/html; charset=utf-8');
 
 echo '<h1>go\DB 2</h1>';
+$params = array(
+    'host'     => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'dbname'   => 'test',
+    'charset'  => 'utf8',
+    '_debug'   => true,
+    '_prefix'  => '',
+);
 
+$db = go\DB\DB::create($params, 'mysql');
 require(__DIR__.'/goDB/autoload.php');
 \go\DB\autoloadRegister();
