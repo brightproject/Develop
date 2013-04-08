3caa9b5a8b03dff0bcbc9aac055f16a1806415df
Brightproject <brightproject@gmail.com>
2013-04-08 16:49:13 +0300

    сменил название

---
+++ BrightWork/engine/controller/applet.php
@@ -0,0 +1,12 @@
+<?php
+   
+ class Apps {
+   function rout_test ()
+     {
+         print 'test';
+     }
+   function rout_index ()
+     {
+        print 'Вывод типичной главной страницы';
+     }
+  }
