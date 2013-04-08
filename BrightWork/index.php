f7cb13719bda68accdd226295c9cacd9dc1a3eae
Brightproject <brightproject@gmail.com>
2013-04-08 16:47:30 +0300

    прописал пути

--- BrightWork/index.php
+++ BrightWork/index.php
@@ -1,3 +1,11 @@
 <?php
-echo "Hello Bright World!!!";
-?>
+	 //Вывод всех ошибок
+	error_reporting(E_ALL); 
+	// defining constants
+	define('DIR_WEB', dirname(__FILE__));
+	define('DIR_ENGINE', dirname(__FILE__).'/engine');
+	define('DIR_CORE', DIR_ENGINE.'/core');
+	define('DIR_CONTR', DIR_ENGINE.'/controller');
+	define('DIR_MODEL', DIR_ENGINE.'/model');
+	define('DIR_VIEW', DIR_ENGINE.'/view');
+	require_once 'application/bootstrap.php';
