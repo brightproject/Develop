13e5d0d0bcee0b0029a9cab361b84fc27cda0449
Brightproject <brightproject@gmail.com>
2013-04-08 16:53:03 +0300

    сменил тип файла utf-8 без bom

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 		$db = new SafeMySQL();
 		//$db = new SafeMysql(array('user' => $user, 'pass' => $pass,'db' => $db, 'charset' => 'cp1251'));
 		$profile .= "<form action=\"index.php\" method=\"post\">";
-		$profile .= "<p><b>Введите имя:</b></p>";
+		$profile .= "<p><b>Р’РІРµРґРёС‚Рµ РёРјСЏ:</b></p>";
 		$profile .= "<p><textarea rows=\"1\" cols=\"45\" name=\"name\"></textarea></p>";
-		$profile .= "<p><input type=\"submit\" value=\"Отправить\"></p>";
+		$profile .= "<p><input type=\"submit\" value=\"РћС‚РїСЂР°РІРёС‚СЊ\"></p>";
 		$profile .= "</form>";
-		$profile .= "Можете написать на почту <a href=\"mailto:obmanka@antispam.com\" onmouseover=\"this.href='mail' + 'to:' + 'sh.magomedow' + '@' + 'yandex' + '.ru'\">e-mail</a>";
+		$profile .= "РњРѕР¶РµС‚Рµ РЅР°РїРёСЃР°С‚СЊ РЅР° РїРѕС‡С‚Сѓ <a href=\"mailto:obmanka@antispam.com\" onmouseover=\"this.href='mail' + 'to:' + 'sh.magomedow' + '@' + 'yandex' + '.ru'\">e-mail</a>";
 		$user = $db->getAll("SELECT * FROM first where name=?s", $_POST['name']);
 		$profile .= "<ul>";
 				 foreach ($user as $m)
