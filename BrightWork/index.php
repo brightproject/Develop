198c8d584695000d930b42614f80b20e27ab5a43
Brightproject <brightproject@gmail.com>
2013-04-08 16:59:15 +0300

    вывод путей

--- BrightWork/index.php
+++ BrightWork/index.php
@@ -9,3 +9,9 @@
 	define('DIR_MODEL', DIR_ENGINE.'/model');
 	define('DIR_VIEW', DIR_ENGINE.'/view');
 	require_once DIR_ENGINE.'/bootstrap.php';
+	echo DIR_WEB.'</br>';
+	echo DIR_ENGINE.'</br>';
+	echo DIR_CORE.'</br>';
+	echo DIR_CONTR.'</br>';
+	echo DIR_MODEL.'</br>';
+	echo DIR_VIEW.'</br>';
