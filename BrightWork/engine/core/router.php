71c350590103f6eb8e951eb0df7ff3e814b070cb
Brightproject <brightproject@gmail.com>
2013-04-08 23:29:15 +0300

    рабочий роутер

--- BrightWork/engine/core/router.php
+++ BrightWork/engine/core/router.php
@@ -8,9 +8,13 @@
      
    function __construct ()
      {
-         $this->routs = explode('/', $_SERVER['REQUEST_URI']); // Разделяем наш запрос
-         if ($this->routs[2] == $this->modules OR !count($this->routs[2])) { // Если передаётся нужный нам объект либо вообще ничего
-         $this->action = $this->routs[3];
+         $this->routs = explode('/', $_SERVER['REQUEST_URI']);
+		 echo "<pre>";
+		 print_r($this->routs);
+		 echo "</pre>";
+		 // Разделяем наш запрос
+         if ($this->routs[4] == $this->modules OR !count($this->routs[4])) { // Если передаётся нужный нам объект либо вообще ничего
+         $this->action = $this->routs[5];
          $this->action = ($this->action == NULL OR !count($this->action)) ? $this->main_action : $this->action;
          $this->get_routs ();
          }
@@ -27,3 +31,23 @@
          }
      
    }
+   
+   // "ловим" строку запроса, превращаем её в масив
+$routeArray = explode('/', $_SERVER['REQUEST_URI']);
+// удаляем пустые элементы массива (элементы образованные начальным и конечным слэшами URI)
+// тут можно было обойтить array_shift и array_pop - но мне способ с foreach кажется более "универсальным"
+$route = array();
+foreach ($routeArray as $value) {
+	if (!empty($value)) {
+		$route[] = trim($value);
+	}
+
+}
+// echo $routeArray[1];
+// echo $routeArray[2];
+// echo $routeArray[3];
+// echo $routeArray[4];
+// вводим в адресную строку всякий бред, смотрим что нам показывают
+echo "<pre>";
+print_r($route);
+echo "</pre>";
