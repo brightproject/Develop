33c5aad86796bc4dfbcc3260d83e343785435704
Brightproject <brightproject@gmail.com>
2013-04-16 11:30:52 +0300

    удаление папки core и всех ссылок

--- BrightWork/engine/bootstrap.php
+++ BrightWork/engine/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 	 require_once DIR_CONTR . '/applet.php'; // Путь к приложениям
-	 require_once DIR_CORE . '/router.php'; // Путь к классу
+	 require_once DIR_CONTR . '/router.php'; // Путь к классу
 	 $routing = new Routing;
--- BrightWork/index.php
+++ BrightWork/index.php
@@ -4,7 +4,6 @@
 	// defining constants
 	define('DIR_WEB', dirname(__FILE__));
 	define('DIR_ENGINE', DIR_WEB.'/engine');
-	define('DIR_CORE', DIR_ENGINE.'/core');
 	define('DIR_CONTR', DIR_ENGINE.'/controller');
 	define('DIR_MODEL', DIR_ENGINE.'/model');
 	define('DIR_VIEW', DIR_ENGINE.'/view');
